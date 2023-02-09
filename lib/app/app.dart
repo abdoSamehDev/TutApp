41dbbbbf30fbd3b8db2142386529579c52730d62
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-09 17:04:01 +0200

    start of the project, creating application class

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  //default constructor
+  // const MyApp({Key? key}) : super(key: key);
+
+  //named constructor
+  const MyApp._internal();
+
+  //singleton or single instance
+  static const MyApp _instance = MyApp._internal();
+
+  //factory
+  factory MyApp() => _instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
