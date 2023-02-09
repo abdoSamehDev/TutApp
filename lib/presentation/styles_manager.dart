6fc0c1333c16ba7238422a8b9bf6e72e43549fb9
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-09 18:49:00 +0200

    creating styles manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,71 @@
+import 'package:advanced_flutter_arabic/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: FontConstant.fontFamily,
+    color: color,
+    fontWeight: fontWeight,
+  );
+}
+
+//regular Style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.regular,
+    color,
+  );
+}
+
+//medium Style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.medium,
+    color,
+  );
+}
+
+//light Style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.light,
+    color,
+  );
+}
+
+//smiBold Style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.semiBold,
+    color,
+  );
+}
+
+//bold Style
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontWeightManager.bold,
+    color,
+  );
+}
