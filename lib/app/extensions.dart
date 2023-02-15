49044a1482b520c36b88621bdd0fdeb4fbbdcf54
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-15 21:44:29 +0200

    added extension functions on String and int

---
+++ lib/app/extensions.dart
@@ -0,0 +1,19 @@
+extension NonNulString on String?{
+  String orEmpty(){
+    if(this == null) {
+      return "";
+    } else{
+      return this!;
+    }
+  }
+}
+
+extension NonNulInt on int?{
+  int orZero(){
+    if(this == null) {
+      return 0;
+    } else{
+      return this!;
+    }
+  }
+}
