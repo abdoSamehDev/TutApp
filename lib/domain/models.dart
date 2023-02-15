aaae812abca489b171f9e34e5a08297414c628fb
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-15 21:37:11 +0200

    added login models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -14,3 +14,28 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderObject, this.currentIndex, this.numOfSlides);
 }
+
+
+//login models
+ class Customer{
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+ }
+
+class Contacts{
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication{
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
+}
