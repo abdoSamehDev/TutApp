8de05494d485f3178f4ec568ce92fb9086a0593c
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-18 08:16:53 +0200

    created network info and added internet connection checker

---
+++ lib/data/network/network_info.dart
@@ -0,0 +1,16 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+abstract class NetworkInfo{
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo{
+
+  final InternetConnectionChecker _internetConnectionChecker;
+
+  NetworkInfoImpl(this._internetConnectionChecker);
+
+  @override
+  // TODO: implement isConnected
+  Future<bool> get isConnected => _internetConnectionChecker.hasConnection;
+}
