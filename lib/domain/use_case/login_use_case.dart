f0bd93dec8ca300cb27e4f0d4a964c4ccb97768f
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-19 10:02:21 +0200

    added login use case

---
+++ lib/domain/use_case/login_use_case.dart
@@ -0,0 +1,25 @@
+import 'package:advanced_flutter_arabic/data/network/failure.dart';
+import 'package:advanced_flutter_arabic/data/network/requests.dart';
+import 'package:advanced_flutter_arabic/domain/model/models.dart';
+import 'package:advanced_flutter_arabic/domain/repository/repository.dart';
+import 'package:advanced_flutter_arabic/domain/use_case/base_use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  final Repository _repository;
+
+  LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(
+      LoginUseCaseInput input) async {
+    return await _repository.login(LoginRequest(input.email, input.password));
+  }
+}
+
+class LoginUseCaseInput {
+  String email;
+  String password;
+
+  LoginUseCaseInput(this.email, this.password);
+}
