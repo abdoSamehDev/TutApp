991d14ad6459f5a70580fb5e83ae06a311c20b5b
Abd El-Rahman Sameh <abdelrahman.sameh.1420@gmail.com>
2023-02-22 00:53:41 +0200

    work on login view

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,3 +1,8 @@
+import 'package:advanced_flutter_arabic/presentation/login/view_model/login_view_model.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/assets_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/strings_manager.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,8 +13,123 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  _bind() {
+    _viewModel.start();
+    _usernameController.addListener(() {
+      _viewModel.setUsername(_usernameController.text);
+    });
+    _passwordController.addListener(() {
+      _viewModel.setUsername(_passwordController.text);
+    });
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                  child: Image(
+                    image: AssetImage(ImageAssets.splashLogo),
+                  ),
+                ),
+                const SizedBox(
+                  height: AppSize.s25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppPadding.p20,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsUsernameValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.text,
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                            label: const Text(AppStrings.username),
+                            hintText: AppStrings.username,
+                            errorText: (snapshot.data == true)
+                                ? null
+                                : AppStrings.usernameError),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: AppSize.s14,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppPadding.p20,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                            label: const Text(AppStrings.password),
+                            hintText: AppStrings.password,
+                            errorText: (snapshot.data == true)
+                                ? null
+                                : AppStrings.passwordError),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: AppSize.s14,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppPadding.p20,
+                  ),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return ElevatedButton(
+                        onPressed: () {
+                          _viewModel.login()
+                        },
+                        child: Text(AppStrings.login),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -2,6 +2,9 @@ class AppStrings{
 
   static const noRouteFound = "No Route Found";
 
+  //Skip Button
+  static const skip = "Skip";
+
   //onBoarding
   static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
   static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
@@ -13,8 +16,16 @@ class AppStrings{
   static const onBoardingBody3 = "Tut app is an awesome flutter application using clean architecture #3";
   static const onBoardingBody4 = "Tut app is an awesome flutter application using clean architecture #4";
 
-  //Skip Button
-  static const skip = "Skip";
+  //login screen
+  static const username = "Username";
+  static const usernameError = "Please Enter Your Username";
+  static const password = "Password";
+  static const passwordError = "Please Enter Your Password";
+  static const login = "Login";
+  static const forgetPassword = "Forget Password";
+  static const registerText = "Not a member? Sign Up";
+
+
 
 
 }
